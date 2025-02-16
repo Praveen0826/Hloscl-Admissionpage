e5ef5f50ab1477c9f62a3315c810c1d6ed668fd4
Praveen0826 <dpraveenkumar008d@gmail.com>
2025-02-16 22:28:39 +0530

    Your commit message

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@
 function Footer() {
   return (
     <div className="flex justify-center my-6">
+      
         <h1>Copyrights ©<span className="text-[#3c7cc9]">HloSkl </span>By <span className="text-[#ffc22c]">NEXCAP</span> </h1>
     </div>
   )
